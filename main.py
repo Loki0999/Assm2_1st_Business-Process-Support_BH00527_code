823ac77fcdd05ca7b15a5b5b7110a4299d9fd22f
LeHoangThai <153346805+Loki0999@users.noreply.github.com>
2024-04-10 22:05:49 +0700

    ASM2

--- main.py
+++ main.py
@@ -13,9 +13,44 @@ def clean_special_characters(text):
     cleaned_text = ''.join(e for e in text if e.isalnum() or e.isspace())
     return cleaned_text
 
+
+
 # Apply the clean_special_characters function to each cell in the DataFrame
 for column in df.columns:
     df[column] = df[column].astype(str).apply(clean_special_characters)
+
+
+
+# Function to add spaces before capitals
+def add_space_before_capitals(text):
+    # Initialize an empty string to store the modified text
+    modified_text = ""
+    # Iterate through each character in the text
+    for i, char in enumerate(text):
+        # If the character is a capital letter and the previous character is not a space, add a space before it
+        if char.isupper() and i > 0 and text[i - 1] != ' ':
+            modified_text += " " + char
+        else:
+            modified_text += char
+    return modified_text.strip()  # Strip leading and trailing spaces
+
+
+# Apply the add_space_before_capitals function to 'Product Name' column
+df['Product Name'] = df['Product Name'].apply(add_space_before_capitals)
+df['Supplier'] = df['Supplier'].apply(add_space_before_capitals)
+df['Category'] = df['Category'].apply(add_space_before_capitals)
+
+
+# Convert letters to uppercase
+df['Supplier'] = df['Supplier'].str.title()
+df['Category'] = df['Category'].str.title()
+df['Product Name'] = df['Product Name'].str.title()
+
+
+
+# Sort the DataFrame by Product ID
+df['Product ID'] = df['Product ID'].astype(int)  # Convert Product ID to integer for sorting
+df = df.sort_values(by='Product ID').reset_index(drop=True)
 
 # Remove duplicate rows
 df.drop_duplicates(inplace=True)
